19236aee4ad5692155f7e17a9c75d27845d37813
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2023-12-20 09:13:21 +0530

    optimize for azrue hosting

--- main.py
+++ main.py
@@ -81,3 +81,6 @@ async def send_email(message: Sponsor):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to send email: {str(e)}")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
